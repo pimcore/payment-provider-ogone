f0a825c7d4b2cef562f9ed642314ee8579b64d3a
dvesh3 <divesh.pahuja@pimcore.com>
2021-01-26 19:31:06 +0100

    initial commit

---
+++ src/OGone/Installer.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Enterprise License (PEL)
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     PEL
+ */
+namespace Pimcore\Bundle\EcommerceFrameworkBundle\OGone;
+
+use Pimcore\Bundle\EcommerceFrameworkBundle\Tools\PaymentProviderInstaller;
+
+class Installer extends PaymentProviderInstaller
+{
+    protected $bricksToInstall = [
+        'PaymentProviderOGone' => 'objectbrick_PaymentProviderOGone_export.json'
+    ];
+}
