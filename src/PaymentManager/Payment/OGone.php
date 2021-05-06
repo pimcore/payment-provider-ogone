965b00a860c7dcc3c6499a154572abbf9bbc3743
fashxp <fashxp@users.noreply.github.com>
2021-05-06 16:09:03 +0000

    Apply php-cs-fixer changes

--- src/PaymentManager/Payment/OGone.php
+++ src/PaymentManager/Payment/OGone.php
@@ -1,15 +1,16 @@
 <?php
+
 /**
  * Pimcore
  *
  * This source file is available under two different licenses:
  * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Enterprise License (PEL)
+ * - Pimcore Commercial License (PCL)
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
 namespace Pimcore\Bundle\EcommerceFrameworkBundle\PaymentManager\Payment;
@@ -372,6 +373,7 @@ class OGone extends AbstractPayment implements \Pimcore\Bundle\EcommerceFramewor
     {
         /* Example fields: EMAIL, "CN", "OWNERADDRESS", "OWNERZIP", "OWNERCITY", etc. */
         $additionalParams = []; //@map onto additional params from config
+
         return $additionalParams;
     }
 
