604a22f269bca34e8da308b6966d2fc57a641869
Olivier Grandmougin <10164678+ogmdev@users.noreply.github.com>
2024-04-19 12:26:59 +0200

    2.x ogone improvements (#9)
    * fix: Use STATUS value from bank response
    
    * feat: Add secret config for ShaIn and ShaOut request/response
    
    * feat: Update Exception message on incorrect additional payment data
    
    * feat: Update SHA OUT Parameters
    
    * chore: Update and apply php-cs-fixer rules
    
    * fix: Pimcore 11 type hint
    
    * fix: Add pimcore/ecommerce-framework-bundle requirement

--- src/OGone/Installer.php
+++ src/OGone/Installer.php
@@ -19,9 +19,9 @@ use Pimcore\Bundle\EcommerceFrameworkBundle\Tools\PaymentProviderInstaller;
 
 class Installer extends PaymentProviderInstaller
 {
-    protected $bricksPath = __DIR__ . '/../../install/objectbrick_sources/';
+    protected string $bricksPath = __DIR__ . '/../../install/objectbrick_sources/';
 
-    protected $bricksToInstall = [
-        'PaymentProviderOGone' => 'objectbrick_PaymentProviderOGone_export.json'
+    protected array $bricksToInstall = [
+        'PaymentProviderOGone' => 'objectbrick_PaymentProviderOGone_export.json',
     ];
 }
